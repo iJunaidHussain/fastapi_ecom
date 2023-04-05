24889841db5edf6ee4a6e874175a35ebecabbe03
Junaid Hussain <junaid.hussain77@yahoo.com>
2023-04-05 20:08:10 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return "Hello World"
+
+
+# if __name__ == "__main__":
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
+    # uvicorn main:app --reload
