9545b5d7d50ccd9d1ac8a89c454d91267fb528f6
Junaid Hussain <junaid.hussain77@yahoo.com>
2023-04-09 19:51:03 +0500

    delete product by id router added

--- app/routers/product.py
+++ app/routers/product.py
@@ -18,7 +18,7 @@ def get_all(db: Session = Depends(get_db)):
 
 
 @router.post('/add', status_code=status.HTTP_201_CREATED)
-def add_product(request: schemas.Product, db: Session = Depends(get_db)):
+def add(request: schemas.Product, db: Session = Depends(get_db)):
     new_product = models.Product(name=request.name, description=request.description,
                                   image_url=request.image_url, category=request.category,
                                     quantity=request.quantity, is_active=request.is_active,
@@ -37,3 +37,14 @@ def get_by_id(id, db: Session = Depends(get_db)):
         raise  HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Product with the id {id} is not available")
     return product  
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT,)
+def delete(id, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Product with the id {id} is not available")
+    db.delete(product)
+    db.commit()    
+    return f"Product with the id {id} Successfully Deleted"
