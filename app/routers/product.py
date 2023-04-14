0e2ebd1ce3e2a1bd5bfd403213de3034d1cd5f0e
Junaid Hussain <junaid.hussain77@yahoo.com>
2023-04-14 20:10:12 +0500

    Search feature added in products

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 
 from .. import schemas, database, models
 
@@ -12,22 +13,19 @@ get_db = database.get_db
 
 
 @router.get('/')
-def get_all(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
-    products = db.query(models.Product).offset(skip).limit(limit).all()
+def get_all(db: Session = Depends(get_db), skip: int = 0, limit: int = 10,
+            q: str = None):
+    query = db.query(models.Product)
+    if q:
+            query = query.filter(
+                or_(
+                models.Product.name.ilike(f'%{q}%'),
+                models.Product.category.ilike(f'%{q}%'),
+                models.Product.description.ilike(f'%{q}%')                
+                )
+            )
+    products = query.offset(skip).limit(limit).all()
     return products
-
-
-@router.post('/add', status_code=status.HTTP_201_CREATED)
-def add(request: schemas.Product, db: Session = Depends(get_db)):
-    new_product = models.Product(name=request.name, description=request.description,
-                                  image_url=request.image_url, category=request.category,
-                                    quantity=request.quantity, is_active=request.is_active,
-                                      price=request.price)
-
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-    return new_product
 
 
 @router.get('/{id}', status_code=200)
@@ -37,6 +35,18 @@ def get(id: int, db: Session = Depends(get_db)):
         raise  HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Product with the id {id} is not available")
     return product  
+
+
+@router.post('/add', status_code=status.HTTP_201_CREATED)
+def add(request: schemas.Product, db: Session = Depends(get_db)):
+    new_product = models.Product(name=request.name, description=request.description,
+                                  image_url=request.image_url, category=request.category,
+                                    quantity=request.quantity, is_active=request.is_active,
+                                      price=request.price)
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
